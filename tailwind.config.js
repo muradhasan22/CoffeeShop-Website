4182d34ce8a77f6051040bb55ca5a49f7b0495cf
Murad Hasan <muradhasan22.com>
2024-10-11 06:53:37 -0700

    list of color set by default in config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     extend: {
       colors: {
         "color-primary": "#01051e",
+        "color-primary-light": "#020726",
+        "color-primary-dark": "#010417",
+        "color-secondary": "#ff7d3b",
+        "color-gray": "#333",
+        "color-white": "fff",
+        "color-blob": "#A427DF"
       }
     },
   },
